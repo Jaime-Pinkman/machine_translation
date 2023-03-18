71d1f98edf8d0964659d0ac236f6ffd0072d0353
Jaime-Pinkman <marathon.our@gmail.com>
2023-03-19 01:25:56 +0300

    refactored vectorizer

--- src/utils/vectorizer.py
+++ src/utils/vectorizer.py
@@ -2,75 +2,77 @@ import numpy as np
 import scipy.sparse
 
 
-def vectorize_texts_sparse(
-    tokenized_texts, word2id, word2freq, mode="tfidf", scale=True
-):
-    assert mode in {"tfidf", "idf", "tf", "bin"}
+class Vectorizer:
+    def __init__(
+        self, tokenizer, vocabulary, mode="ltfidf", scale="minmax", use_sparse=False
+    ):
+        self.tokenizer = tokenizer
+        self.vocab = vocabulary
+        assert mode in {"ltfidf", "tfidf", "idf", "tf", "bin"}
+        assert scale in {"minmax", "std", None}
+        self.mode = mode
+        self.scale = scale
+        self.use_sparse = use_sparse
 
-    result = scipy.sparse.dok_matrix(
-        (len(tokenized_texts), len(word2id)), dtype="float32"
-    )
-    for text_i, text in enumerate(tokenized_texts):
-        for token in text:
-            if token in word2id:
-                result[text_i, word2id[token]] += 1
+    def fit(self, texts):
+        if self.vocab.word2id is None or self.vocab.word2freq is None:
+            tokenized_texts = self.tokenizer.tokenize_corpus(texts)
+            self.vocab.build(tokenized_texts)
 
-    if mode == "bin":
-        result = (result > 0).astype("float32")
+    def transform(self, texts):
+        if self.vocab.word2id is None or self.vocab.word2freq is None:
+            raise ValueError("Vectorizer has not been fitted yet.")
+        tokenized_texts = self.tokenizer.tokenize_corpus(texts)
+        if self.use_sparse:
+            result = scipy.sparse.dok_matrix(
+                (len(tokenized_texts), len(self.vocab.word2id)), dtype="float32"
+            )
+        else:
+            result = np.zeros((len(tokenized_texts), len(self.vocab.word2id)))
 
-    elif mode == "tf":
-        result = result.tocsr()
-        result = result.multiply(1 / result.sum(1))
+        for text_i, text in enumerate(tokenized_texts):
+            for token in text:
+                if token in self.vocab.word2id:
+                    result[text_i, self.vocab.word2id[token]] += 1
 
-    elif mode == "idf":
-        result = (result > 0).astype("float32").multiply(1 / word2freq)
+        if self.mode == "bin":
+            result = (result > 0).astype("float32")
 
-    elif mode == "tfidf":
-        result = result.tocsr()
-        result = result.multiply(1 / result.sum(1))
-        result = result.multiply(1 / word2freq)
+        elif self.mode == "tf":
+            if self.use_sparse:
+                result = result.tocsr()
+            result = result.multiply(1 / result.sum(1))
 
-    if scale:
-        result = result.tocsc()
-        result -= result.min()
-        result /= result.max() + 1e-6
+        elif self.mode == "idf":
+            if self.use_sparse:
+                result = result.tocsc()
+            result = (result > 0).astype("float32").multiply(1 / self.vocab.word2freq)
 
-    return result.tocsr()
+        elif self.mode == "tfidf":
+            if self.use_sparse:
+                result = result.tocsr()
+            result = result.multiply(1 / result.sum(1))
+            result = result.multiply(1 / self.vocab.word2freq)
 
+        elif self.mode == "ltfidf":
+            if self.use_sparse:
+                result = result.tocsr()
+            result = np.log(result * (1 / result.sum(1))[:, np.newaxis] + 1)
+            result = result * (1 / self.vocab.word2freq)
 
-def vectorize_texts(tokenized_texts, word2id, word2freq, mode="tfidf", scale="minmax"):
-    assert mode in {"ltfidf", "tfidf", "idf", "tf", "bin"}
-    assert scale in {"minmax", "std", None}
+        if self.scale == "minmax":
+            if self.use_sparse:
+                result = result.tocsc()
+            result -= result.min()
+            result /= result.max() + 1e-6
 
-    result = np.zeros((len(tokenized_texts), len(word2id)))
-    for text_i, text in enumerate(tokenized_texts):
-        for token in text:
-            if token in word2id:
-                result[text_i, word2id[token]] += 1
+        elif self.scale == "std":
+            if self.use_sparse:
+                result = result.tocsc()
+            result -= result.mean(0)
+            result /= result.std(0, ddof=1)
 
-    if mode == "bin":
-        result = (result > 0).astype("float32")
+        if self.use_sparse:
+            result = result.tocsr()
 
-    elif mode == "tf":
-        result = result.multiply(1 / result.sum(1))
-
-    elif mode == "idf":
-        result = (result > 0).astype("float32").multiply(1 / word2freq)
-
-    elif mode == "tfidf":
-        result = result.multiply(1 / result.sum(1))
-        result = result.multiply(1 / word2freq)
-
-    elif mode == "ltfidf":
-        result = np.log(result * (1 / result.sum(1))[:, np.newaxis] + 1)
-        result = result * (1 / word2freq)
-
-    if scale == "minmax":
-        result -= result.min()
-        result /= result.max() + 1e-6
-
-    if scale == "std":
-        result -= result.mean(0)
-        result /= result.std(0, ddof=1)
-
-    return result
+        return result
