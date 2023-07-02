f720c377472154b877be2a5288c5e4d8eb0579c9
Isangulov Marat <37271333+Jaime-Pinkman@users.noreply.github.com>
2023-07-02 22:03:16 +0800

    Feature/refactor structure (#4)
    * added init files to import models
    
    * refactored imports
    
    * refactored fixtures with conftest.py

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+from .sentence_classifier import (
+    Tokenizer,
+    Vocabulary,
+    VectorizerFactory,
+    BaseVectorizer,
+)
+from .transformers import Transformer
+
+__all__ = [
+    "Tokenizer",
+    "Vocabulary",
+    "VectorizerFactory",
+    "BaseVectorizer",
+    "Transformer",
+]
---
+++ src/models/sentence_classifier/__init__.py
@@ -0,0 +1,9 @@
+from .tokenizer import Tokenizer, Vocabulary
+from .vectorizer import VectorizerFactory, BaseVectorizer
+
+__all__ = [
+    "Tokenizer",
+    "Vocabulary",
+    "VectorizerFactory",
+    "BaseVectorizer",
+]
