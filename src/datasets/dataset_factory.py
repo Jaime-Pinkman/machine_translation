3230f78298c2c2cc06398842c27b83975010ab4b
Marat Isangulov <marathon.our@gmail.com>
2023-07-14 00:06:42 +0300

    refactored init of factory

--- src/datasets/dataset_factory.py
+++ src/datasets/dataset_factory.py
@@ -9,30 +9,20 @@ from .sparse_dataset import SparseDataset
 
 
 class BaseDatasetFactory(ABC):
-    def __init__(
-        self,
-        features: np.ndarray | sparse._dok.dok_matrix,
-        targets: np.ndarray | sparse._dok.dok_matrix,
-    ):
-        self.features = features
-        self.targets = targets
-
     @abstractmethod
-    def create_dataset(self) -> BaseDataset:
+    def create_dataset(
+        self, features: np.ndarray | sparse._dok.dok_matrix, targets: np.ndarray
+    ) -> BaseDataset:
         pass
 
 
-class SparseDatasetFactory(BaseDatasetFactory):
-    def create_dataset(self) -> SparseDataset:
-        return SparseDataset(
-            features=self.features,
-            targets=self.targets,
-        )
-
-
 class DenseDatasetFactory(BaseDatasetFactory):
-    def create_dataset(self) -> DenseDataset:
-        return DenseDataset(
-            features=self.features,
-            targets=self.targets,
-        )
+    def create_dataset(self, features: np.ndarray, targets: np.ndarray) -> DenseDataset:
+        return DenseDataset(features=features, targets=targets)
+
+
+class SparseDatasetFactory(BaseDatasetFactory):
+    def create_dataset(
+        self, features: sparse._dok.dok_matrix, targets: np.ndarray
+    ) -> SparseDataset:
+        return SparseDataset(features=features, targets=targets)
