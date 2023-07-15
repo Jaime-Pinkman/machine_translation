7a97644e481366618e8c0d61cb32ba94ab39b341
Marat Isangulov <marathon.our@gmail.com>
2023-07-16 02:38:33 +0300

    added configs for hyper parameters

---
+++ src/utils/load_params.py
@@ -0,0 +1,15 @@
+import yaml
+from src.config.config import default_params
+
+
+def load_params(config_file=None):
+    if config_file is None:
+        return default_params
+    else:
+        with open(config_file, "r") as file:
+            params = yaml.safe_load(file)
+        return {
+            "learning_rate": float(params["learning_rate"]),
+            "batch_size": int(params["batch_size"]),
+            "num_epochs": int(params["num_epochs"]),
+        }
